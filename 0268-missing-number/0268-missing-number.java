330b707b0490a44575ea2a4c80b7fc826abbd219
aloksoni58 <alokanilverma80@gmail.com>
2026-09-02 18:14:50 +0530

    Time: 0 ms (100%), Space: 47 MB (95.68%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length;
+        int totalSum = n * (n+1)/2;
+        int numsSum = 0;
+        for(int i : nums) {
+            numsSum+=i;
+        }
+
+        return totalSum-numsSum;
+    }
+}
